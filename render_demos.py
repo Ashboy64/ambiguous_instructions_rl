87a303d954ed45564cd0f2690a795159b8f30bf8
Kyle Beltran Hatch <khatch@DNa8087dc.SUNet>
2022-02-28 20:36:23 -0800

    render policies

--- render_demos.py
+++ render_demos.py
@@ -3,7 +3,8 @@
 """
 Visualize the performance of a model on a given environment.
 """
-
+import os
+import numpy as np
 import argparse
 import gym
 import time
@@ -11,12 +12,30 @@ import cv2
 
 import babyai.utils as utils
 
+from wrappers import AmbiguousInstructionsWrapper, NonsenseInstructionsWrapper
+
 """
 python3 -u render_demos.py \
 --env BabyAI-GoToLocal-v0 \
 --demos /iris/u/khatch/CS224N/ambiguous_instructions_rl/test_data/BabyAI-GoToLocal-v0 \
 --demos-origin agent \
 --n-episodes 1
+
+python3 -u render_demos.py \
+--env BabyAI-PickupLoc-v0 \
+--model BabyAI-PickupLoc-v0_nonsense_ppo_bow_endpool_res_gru_mem_seed1_22-02-26-22-07-48 \
+--nonsense \
+--n-episodes 5
+
+
+python3 -u render_demos.py \
+--env BabyAI-PutNextLocalS6N4-v0 \
+--model BabyAI-PutNextLocalS6N4-v0_nonsense_ppo_bow_endpool_res_gru_mem_seed1_22-02-26-22-09-17 \
+--nonsense \
+--n-episodes 5
+
+
+
 """
 
 # Parse arguments
@@ -38,8 +57,15 @@ parser.add_argument("--pause", type=float, default=0.1,
                     help="the pause between two consequent actions of an agent")
 parser.add_argument("--manual-mode", action="store_true", default=False,
                     help="Allows you to take control of the agent at any point of time")
+
 parser.add_argument("--n-episodes", type=int, default=1,
                     help="How many episodes from the demo file to render")
+parser.add_argument("--nonsense", "-n", action="store_true", default=False,
+                    help="Add NonsenseInstructionsWrapper")
+parser.add_argument("--ambiguous", "-a", action="store_true", default=False,
+                    help="Add AmbiguousInstructionsWrapper")
+parser.add_argument("--prob-ambiguous", type=float, default=0.5,
+                    help="Probability that the env will generate ambiguous instructions each episode")
 args = parser.parse_args()
 
 action_map = {
@@ -66,17 +92,26 @@ utils.seed(args.seed)
 env = gym.make(args.env)
 env.seed(args.seed)
 
+if args.ambiguous:
+    env = AmbiguousInstructionsWrapper(env, prob_ambiguous=args.prob_ambiguous)
+elif args.nonsense:
+    env = NonsenseInstructionsWrapper(env)
+
 global obs
 obs = env.reset()
 print("Mission: {}".format(obs["mission"]))
+if args.ambiguous or args.nonsense:
+    print("True mission: {}".format(env.true_mission))
 
 # Define agent
 agent = utils.load_agent(env, args.model, args.demos, args.demos_origin, args.argmax, args.env)
 
 
+def save_images(images, model_name, episode_num):
+    episode_images_dir = os.path.join("rendered_demos", model_name, f"ep-{episode_num}")
 
-def save_images(images, env_name, ep_number):
-    episode_images_dir = os.path.join("rendered_demos", env_name, ep_number)
+    if not os.path.isdir(episode_images_dir):
+        os.makedirs(episode_images_dir)
 
     for i, image in enumerate(images):
         image = np.flip(image, axis=-1)
@@ -84,36 +119,36 @@ def save_images(images, env_name, ep_number):
         cv2.imwrite(image_file, image)
 
 
-def render_episodes():
-    images = []
-    step = 0
-    episode_num = 0
-    while True:
-        img = env.render("rgb_array")
-        images.append(img)
+images = []
+step = 0
+episode_num = 1
+while True:
+    img = env.render("rgb_array")
+    images.append(img)
 
-        result = agent.act(obs)
-        obs, reward, done, _ = env.step(result['action'])
-        agent.analyze_feedback(reward, done)
-        if 'dist' in result and 'value' in result:
-            dist, value = result['dist'], result['value']
-            dist_str = ", ".join("{:.4f}".format(float(p)) for p in dist.probs[0])
-            print("step: {}, mission: {}, dist: {}, entropy: {:.2f}, value: {:.2f}".format(
-                step, obs["mission"], dist_str, float(dist.entropy()), float(value)))
-        else:
-            print("step: {}, mission: {}".format(step, obs['mission']))
-        if done:
-            print("Reward:", reward)
+    result = agent.act(obs)
+    obs, reward, done, _ = env.step(result['action'])
+    agent.analyze_feedback(reward, done)
+    if 'dist' in result and 'value' in result:
+        dist, value = result['dist'], result['value']
+        dist_str = ", ".join("{:.4f}".format(float(p)) for p in dist.probs[0])
+        # print("step: {}, mission: {}, dist: {}, entropy: {:.2f}, value: {:.2f}".format(
+        #     step, obs["mission"], dist_str, float(dist.entropy()), float(value)))
+    else:
+        print("step: {}, mission: {}".format(step, obs['mission']))
+    if done:
+        # print("Reward:", reward)
+        print("Episode: {}, Mission: {}, True Mission: {}, Return: {}".format(episode_num, obs['mission'], env.true_mission, reward))
 
-            save_images(images, env_name, ep_number)
-            images = []
+        save_images(images, args.model, episode_num)
+        images = []
 
-            episode_num += 1
-            env.seed(args.seed + episode_num)
-            obs = env.reset()
-            agent.on_reset()
-            step = 0
-            if episode_num >= args.n_episodes:
-                break
-        else:
-            step += 1
+        episode_num += 1
+        env.seed(args.seed + episode_num)
+        obs = env.reset()
+        agent.on_reset()
+        step = 0
+        if episode_num > args.n_episodes:
+            break
+    else:
+        step += 1
