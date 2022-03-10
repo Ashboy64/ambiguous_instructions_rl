51a569d895fb4abdf1e3914de253d3ca1ebc8557
Ashboy64 <ashish.arartc@gmail.com>
2022-03-09 19:11:03 -0800

    rl penalties and other changes

--- test_ambiguity.py
+++ test_ambiguity.py
@@ -1,24 +1,47 @@
 """
 Visualize start state, instruction, and ambiguous version for a bunch 
 of different episodes for the specified environment.
+
+Environments that seem to contain decent amount of ambiguity:
+- GoToLocal
+- GoToSeqS5R2 (?)
+- MiniBossLevel (?)
+- PickupLoc
+- PutNextLocal (maybe S6N4 version)
+- Maybe SynthS5R2
+- ActionObjDoor
+- GoToObjDoor
+- OpenDoorLoc
+
+Final List:
+- GoToLocal
+- PickupLoc
+- GoToObjDoor (a lot like GoToLocal, except more rooms as distractors?)
+- Can try PutNextLocalS6N4 (like PutNextLocal but smaller grid so maybe easier to learn, but not as ambiguous)
+- OpenDoorLoc
+
+
 """
 
 
 import gym 
 import babyai
 import time
+import argparse
 from ambiguity import make_ambiguous
 from babyai.levels.levelgen import *
 
 
-# ENV_NAME = "BabyAI-GoToLocal-v0"
-ENV_NAME = "BabyAI-GoTo-v0"
+parser = argparse.ArgumentParser()
+parser.add_argument("--env", required=True,
+                    help="name of the environment to be run (REQUIRED)")
+args = parser.parse_args()
 
 
 def main():
-    print(level_dict)
+    # print(level_dict)
 
-    env = gym.make(ENV_NAME)
+    env = gym.make(f"BabyAI-{args.env}-v0")
     env.render("human")
 
     for i in range(10):
